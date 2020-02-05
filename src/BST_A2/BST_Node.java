fb269f6137d545ccb81d915bb434f24a5faece84
msokol98 <msokol@live.unc.edu>
2020-02-05 13:56:33 -0500

    added node class

---
+++ src/BST_A2/BST_Node.java
@@ -0,0 +1,28 @@
+package BST_A2;
+
+public class BST_Node {
+  String data;
+  BST_Node left;
+  BST_Node right;
+  
+  BST_Node(String data){ this.data=data; }
+
+  // --- used for testing  ----------------------------------------------
+  //
+  // leave these 3 methods in, as is
+
+  public String getData(){ return data; }
+  public BST_Node getLeft(){ return left; }
+  public BST_Node getRight(){ return right; }
+
+  // --- end used for testing -------------------------------------------
+  
+  // --------------------------------------------------------------------
+  // you may add any other methods you want to get the job done
+  // --------------------------------------------------------------------
+  
+  public String toString(){
+    return "Data: "+this.data+", Left: "+((this.left!=null)?left.data:"null")
+            +",Right: "+((this.right!=null)?right.data:"null");
+  }
+}
